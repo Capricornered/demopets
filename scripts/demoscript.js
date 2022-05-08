99d8c3383805c6b43ed7f2946175d3c2f194eb8f
Capricornered <98843600+Capricornered@users.noreply.github.com>
2022-05-08 17:18:25 -0400

    Add files via upload

---
+++ scripts/demoscript.js
@@ -0,0 +1,257 @@
+function openNav() {
+    document.getElementById("slide").style.marginLeft = "0";
+}  
+function closeNav() {
+    document.getElementById("slide").style.marginLeft = "-100%";
+}
+
+function openSub1() {
+    var x = document.getElementById("sub1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.animation = "openUp 0.3s";
+    }
+    var x = document.getElementById("sub2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub4");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub5");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+  } 
+function openSub2() {
+    var x = document.getElementById("sub2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.animation = "openUp 0.3s";
+    }
+    var x = document.getElementById("sub1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub4");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub5");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+} 
+function openSub3() {
+    var x = document.getElementById("sub3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.animation = "openUp 0.3s";
+    }
+        var x = document.getElementById("sub1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub4");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub5");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+} 
+function openSub4() {
+    var x = document.getElementById("sub4");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.animation = "openUp 0.3s";
+    }
+    var x = document.getElementById("sub1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub5");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+} 
+function openSub5() {
+    var x = document.getElementById("sub5");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.animation = "openUp 0.3s";
+    }
+    var x = document.getElementById("sub1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("sub4");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+} 
+function openDown1() {
+    var x = document.getElementById("down1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.animation = "openUp 0.3s";
+    }
+    var x = document.getElementById("down2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down4");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down5");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+}
+function openDown2() {
+    var x = document.getElementById("down2");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.animation = "openUp 0.3s";
+    }
+    var x = document.getElementById("down1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down4");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down5");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+} 
+function openDown3() {
+    var x = document.getElementById("down3");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.animation = "openUp 0.3s";
+    }
+    var x = document.getElementById("down1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down4");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down5");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+} 
+function openDown4() {
+    var x = document.getElementById("down4");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.animation = "openUp 0.3s";
+    }
+    var x = document.getElementById("down1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down5");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+} 
+function openDown5() {
+    var x = document.getElementById("down5");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+      x.style.animation = "openUp 0.3s";
+    }
+    var x = document.getElementById("down1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    var x = document.getElementById("down4");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+}
